07249d1f32a0f70b75c730d27e7ebdeb712c69a3
rafael.guardeno <rafael.guardeno.marquez@gmail.com>
2020-08-16 16:06:26 +0200

    Finish: login form layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,9 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class AppComponent {
   title = 'Authentication App';
+
+  emailErrorMessage = "Error with the email";
+  passwordErrorMessage = "Error with the password";
 
   loginForm =  new FormGroup({
     email: new FormControl(null,  {
@@ -19,4 +23,46 @@ export class AppComponent {
       validators: [Validators.required, Validators.minLength(6)],
     })
   });
+
+  showEmailError() {
+    const emailFormControl: AbstractControl = this.loginForm.get('email');
+    const errors = emailFormControl.errors;
+
+    if (errors)
+      this.setEmailErrorMessage(errors);
+
+    return !emailFormControl.valid && emailFormControl.touched;
+  }
+
+  setEmailErrorMessage(errors) {
+    if (errors.required) {
+      this.emailErrorMessage = "Email is required"
+    } else if (errors.email) {
+      this.emailErrorMessage = "Email must be a valid email address"
+    }
+  }
+
+  showPasswordError() {
+    const passwordFormControl: AbstractControl = this.loginForm.get('password');
+    const errors = passwordFormControl.errors;
+
+    if (errors)
+      this.setPasswordErrorMessage(errors);
+
+    return !this.loginForm.get('password').valid && this.loginForm.get('password').touched;
+  }
+
+  setPasswordErrorMessage(errors) {
+    console.log(errors);
+
+    if (errors.required)
+      this.passwordErrorMessage = "Password is required";
+    else if (errors.minlength)
+      this.passwordErrorMessage = "Password must be at least 6 characters long ";
+  }
+
+  logIn() {
+    this.loginForm.markAllAsTouched();
+    if (this.loginForm.invalid) return;
+  }
 }
