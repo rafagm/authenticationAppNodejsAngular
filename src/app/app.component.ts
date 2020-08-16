54bf7d3efaa69e8877ab92d02b5b822b845f6758
rafael.guardeno <rafael.guardeno.marquez@gmail.com>
2020-08-16 14:47:22 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'login';
+  title = 'Authentication App';
+
+  loginForm =  new FormGroup({
+    email: new FormControl(null,  {
+      updateOn: "blur",
+      validators: [Validators.required, Validators.email]
+    }),
+    password: new FormControl(null, {
+      updateOn: "blur",
+      validators: [Validators.required, Validators.minLength(6)],
+    })
+  });
 }
